53ea3368875ab14e4af92c0aca933d5204fa6ba6
Tin <tin.margetic@fer.hr>
2023-12-16 12:39:14 +0100

    promijenjene slike

--- sw.js
+++ sw.js
@@ -1,20 +1,79 @@
+const filesToCache = [
+    "/",
+    "manifest.json",
+    "index.html",
+    "offline.html",
+    "404.html"
+];
+
+const staticCacheName = "static-cache-v1";
+
 self.addEventListener('install', event => {
-    console.log('V1 installing…');
-    // cache a cat SVG
+    console.log("Attempting to install service worker and cache static assets");
     event.waitUntil(
-        caches.open('static-v1').then(cache => cache.add('./cat.svg'))
+        caches.open(staticCacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
     );
 });
 
 self.addEventListener('activate', event => {
-    console.log('Activated, V1 now ready to handle fetches!');
+    console.log(
+        "*************************************************************************************"
+    );
+    console.log(
+        "******************   Activating new service worker... *******************************"
+    );
+    console.log(
+        "*************************************************************************************"
+    );
+
+    const cacheWhitelist = [staticCacheName];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+
 });
 
 self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
-    // serve the cat SVG from the cache if the request is
-    // same-origin and the path is '/dog.svg'
-    if (url.origin == location.origin && url.pathname == '/dog.svg') {
-        event.respondWith(caches.match('/cat.svg'));
-    }
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then((response) => {
+                if (response) {
+                    console.log("Found " + event.request.url + " in cache!");
+                    return response;
+                }
+                console.log(
+                    "----------------->> Network request for ",
+                    event.request.url
+                );
+                return fetch(event.request).then((response) => {
+                    console.log("response.status = " + response.status);
+                    if (response.status === 404) {
+                        return caches.match("404.html");
+                    }
+                    return caches.open(staticCacheName).then((cache) => {
+                        console.log(">>> Caching: " + event.request.url);
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                });
+            })
+            .catch((error) => {
+                console.log("Error", event.request.url, error);
+                // ovdje možemo pregledati header od zahtjeva i možda vratiti različite fallback sadržaje
+                // za različite zahtjeve - npr. ako je zahtjev za slikom možemo vratiti fallback sliku iz cachea
+                // ali zasad, za sve vraćamo samo offline.html:
+                return caches.match("offline.html");
+            })
+    );
 });
