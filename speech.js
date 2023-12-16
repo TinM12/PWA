42b07ab3b9915779ef3f3fdf77fa2f2ee890477b
Tin <tin.margetic@fer.hr>
2023-12-16 13:24:27 +0100

    dodaj gumb za stop

--- speech.js
+++ speech.js
@@ -1,28 +1,34 @@
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 
 const recognition = new SpeechRecognition();
-const diagnostic = document.querySelector(".output");
-
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+const output = document.querySelector(".output");
+let isRecording = false;
+
 document.getElementById('startRecognition').onclick = () => {
-    recognition.start();
-    console.log('Recording started');
+    if (!isRecording) {
+        recognition.start();
+        isRecording = true;
+        console.log('Recording started');
+    }
 };
 
-recognition.onresult = (event) => {
-    const speechResult = event.results[0][0].transcript;
-    diagnostic.textContent = `You said: ${speechResult}`;
+document.getElementById('stopRecording').onclick = () => {
+    if (isRecording) {
+        recognition.stop();
+        isRecording = false;
+        console.log('Recording stopped');
+    }
 };
 
-// Handle errors
-    recognition.onerror = (event) => {
+recognition.onerror = (event) => {
     console.error('Speech recognition error occurred: ', event.error);
 };
-
-// Handle the end of speech recognition
-    recognition.onend = () => {
+  
+recognition.onend = () => {
     console.log('Speech recognition ended');
+    isRecording = false; // Ensure the recording flag is updated on end
 };
